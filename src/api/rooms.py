45680b244ec969568778163178b2691fa6e95cd8
Noobmaster005 <ayb.saidahmedov@gmail.com>
2025-01-03 21:50:26 +0300

    последний кооммит

--- src/api/rooms.py
+++ src/api/rooms.py
@@ -1,57 +1,71 @@
-from fastapi import APIRouter, HTTPException
-from src.schemas.rooms import Rooms, RoomsAdd, RoomsUpdate
+from fastapi import APIRouter, Body, HTTPException
+from src.schemas.rooms import Rooms, RoomsAdd, RoomsAddRequest, RoomsPatch, RoomsPatchRequest
 from src.repositories.rooms import RoomsRepository
 from src.database import async_session_maker
-from src.repositories.base import BaseRepository
 
 
-router = APIRouter(prefix="/rooms", tags=["Номера"])
 
-@router.get("/")
+router = APIRouter(prefix="/hotels", tags=["Номера"])
+
+@router.get("/{hotel_id}/rooms")
 async def get_all_rooms():
     async with async_session_maker() as session:
         rooms = await RoomsRepository(session).get_all()
         return rooms
 
-@router.get("/{id}", response_model=Rooms)
-async def get_room(id: int):
+@router.get("/{hotel_id}/rooms/{room_id}")
+async def get_room(hotel_id: int, room_id: int):
     async with async_session_maker() as session:
-        room = await RoomsRepository(session).get_one_or_none(id=id)
+        room = await RoomsRepository(session).get_one_or_none(id=room_id)
         if not room:
             raise HTTPException(status_code=404, detail="Комната не найдена")
         return room
 
-@router.post("/", response_model=Rooms)
-async def create_room(room_data: RoomsAdd):
+@router.post("/{hotel_id}/rooms")
+async def create_room(hotel_id: int, room_data: RoomsAddRequest = Body()):
+    _room_data = RoomsAdd(hotel_id = hotel_id, **room_data.model_dump())
     async with async_session_maker() as session:
-        new_room = await RoomsRepository(session).add(room_data)
-        return new_room
+        room = await RoomsRepository(session).add(room_data)
+        await session.commit()
+    
+    return {"status": "OK", "data": room}
 
-@router.put("/{id}", response_model=Rooms)
-async def replace_room(id: int, room_data: RoomsAdd):
+@router.put("/{hotel_id}/rooms/{room_id}")
+async def replace_room(hotel_id, room_id: int, room_data: RoomsAddRequest):
+    _room_data = RoomsAdd(hotel_id = hotel_id, **room_data.model_dump())
     async with async_session_maker() as session:
         room = await RoomsRepository(session).get_one_or_none(id=id)
         if not room:
             raise HTTPException(status_code=404, detail="Комната не найдена")
         
-        await RoomsRepository(session).edit(room_data, id=id)
+        await RoomsRepository(session).edit(_room_data, id=room_id)
         updated_room = await RoomsRepository(session).get_one_or_none(id=id)
-        return updated_room
+        await session.commit()
+    
+    return updated_room
 
-@router.patch("/{id}", response_model=Rooms)
-async def update_room(id: int, room_data: RoomsUpdate):
+@router.patch("/{hotel_id}/rooms/{room_id}")
+async def partially_edit_room(
+        hotel_id: int,
+        room_id: int, 
+        room_data: RoomsPatchRequest
+):
+       
+    _room_data = RoomsPatch(hotel_id = hotel_id, **room_data.model_dump(exclude_unset=True))
     async with async_session_maker() as session:
-        room = await RoomsRepository(session).get_one_or_none(id=id)
-        if not room:
-            raise HTTPException(status_code=404, detail="Комната не найдена")
 
-        await RoomsRepository(session).edit(room_data, exclude_unset=True, id=id)
-        updated_room = await RoomsRepository(session).get_one_or_none(id=id)
-        return updated_room
+        await RoomsRepository(session).edit(
+            _room_data, 
+            exclude_unset=True, 
+            id=room_id, 
+            hotel_id=hotel_id)
+        await session.commit()
 
-@router.delete("/{id}")
-async def delete_room(id: int):
+        return {"status": "OK"}
+
+@router.delete("/{hotel_id}/rooms/{room_id}")
+async def delete_room(hotel_id: int, room_id: int):
     async with async_session_maker() as session:
-        await RoomsRepository(session).delete(id=id)
+        await RoomsRepository(session).delete(id=room_id,hotel_id=hotel_id)
         await session.commit()
     return {"status": "OK"}
--- src/schemas/rooms.py
+++ src/schemas/rooms.py
@@ -1,21 +1,30 @@
-from pydantic import BaseModel, Field, ConfigDict
-
-
-class RoomsAdd(BaseModel):
+from pydantic import BaseModel, ConfigDict
+class RoomAddRequest(BaseModel):
     title: str
-    description: str
-    price: float
+    description: str | None = None
+    price: int
     quantity: int
 
+class RoomAdd(BaseModel):
+    hotel_id: int
+    title: str
+    description: str | None = None
+    price: int
+    quantity: int
 
-class Rooms(RoomsAdd):
+class Room(RoomAdd):
     id: int
-
     model_config = ConfigDict(from_attributes=True)
 
+class RoomPatchRequest(BaseModel):
+    title: str | None = None
+    description: str | None = None
+    price: int | None = None
+    quantity: int | None = None
 
-class RoomsUpdate(BaseModel):
-    title: str | None = Field(None)
-    description: str | None = Field(None)
-    price: float | None = Field(None)
-    quantity: int | None = Field(None)
+class RoomPatch(BaseModel):
+    hotel_id: int | None = None
+    title: str | None = None
+    description: str | None = None
+    price: int | None = None
+    quantity: int | None = None
